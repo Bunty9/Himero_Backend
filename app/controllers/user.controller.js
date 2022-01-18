b150f999fa90d0ce67ab5359cac4375c07b0bfa1
Bunty9 <cbipin2000@gmail.com>
2022-01-19 01:25:26 +0530

    control

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,8 +2,30 @@ exports.allAccess = async (req, res) => {
     await res.status(200).send("All Public Content.");
 };
 
-exports.userBoard = async (req, res) => {
-    await res.status(200).send("User Content.");
+exports.userBoard = async function (req, res, next) {
+    var data = [];
+    const user = await User.findById(req.body.userid).exec();
+    for (i = 0; i < user.house.length; i++) {
+        const house = await House.find().where("_id").in(user.house[i]).exec();
+        const deviceIDS = house.map((house) => house.device).flat();
+        if (house.length > 0) {
+            data.push(house);
+        }
+        var devices = [];
+        for (j = 0; j < deviceIDS.length; j++) {
+            const device = await Device.find()
+                .where("_id")
+                .in(deviceIDS[j])
+                .exec();
+            devices.push(device[0]);
+
+            console.log(devices);
+        }
+        data[i].push(devices);
+    }
+    console.log(data);
+    res.send(data).status(200);
+    next();
 };
 
 exports.adminBoard = async (req, res) => {
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -16,38 +16,7 @@ module.exports = function (app) {
 
     app.get("/api/all", controller.allAccess);
 
-    app.post(
-        "/api/user",
-        [authJwt.verifyToken],
-        async function (req, res, next) {
-            var data = [];
-            const user = await User.findById(req.body.userid).exec();
-            for (i = 0; i < user.house.length; i++) {
-                const house = await House.find()
-                    .where("_id")
-                    .in(user.house[i])
-                    .exec();
-                const deviceIDS = house.map((house) => house.device).flat();
-                if (house.length > 0) {
-                    data.push(house);
-                }
-                var devices = [];
-                for (j = 0; j < deviceIDS.length; j++) {
-                    const device = await Device.find()
-                        .where("_id")
-                        .in(deviceIDS[j])
-                        .exec();
-                    devices.push(device[0]);
-
-                    console.log(devices);
-                }
-                data[i].push(devices);
-            }
-            console.log(data);
-            res.send(data).status(200);
-            next();
-        }
-    );
+    app.post("/api/user", [authJwt.verifyToken], controller.userAccess);
 
     app.post("/api/admin", [authJwt.verifyToken], controller.adminBoard);
 };
